9d4acb00a54d7919878050b6619c5a2e4a883a89
Erwan Tremiot <erwan.tremiot@hotmail.fr>
2014-01-25 23:38:35 +0100

    Added Grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      js: {
+        files: '*.js',
+        tasks: ['js'],
+        options: {
+          interrupt: true,
+        },
+      },
+      coffee: {
+        files: '*.coffee',
+        tasks: ['coffee'],
+        options: {
+          interrupt: true,
+        },
+      },
+    },
+    jshint: {
+      all: ['Gruntfile.js', '*.js']
+    },
+    coffeecompile: {
+      app: {
+        files: {
+          '*.js': '*.coffee'
+        }
+      },
+    },
+    coffeelint: {
+      app: ['*.coffee'],
+      options: {
+        arrow_spacing: true,
+        colon_assignment_spacing: true
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default task(s).
+  grunt.registerTask('default', ['coffeelint', 'coffee:compile', 'jshint']);
+  grunt.registerTask('coffee', ['coffeelint', 'coffeecompile']);
+  grunt.registerTask('js', ['jshint']);
+
+};
